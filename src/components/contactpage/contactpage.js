beabcc29a8c9fd0852d386966c2c36d5e7cec416
hassanzaid-bit <hzaid.hz@gmail.com>
2021-12-08 12:13:03 +0300

    Fixed typography

--- src/components/contactpage/contactpage.js
+++ src/components/contactpage/contactpage.js
@@ -1,7 +1,9 @@
+import "./contactpage.css"
+
 const Contactpage = () => {
     return ( 
-        <div className='my-52 text-left '>
-             <p className='text-white text-7xl md:text-9xl font-semibold'>Want to talk about a potential project or collaboration ?Awesome! Let's grab a virtual coffee and you can tell me all about it </p>
+        <div className='contactmepage text-left '>
+             <p className=' coffee  text-white font-semibold'>Want to talk about a potential project or collaboration ?Awesome! Let's grab a virtual coffee and you can tell me all about it </p>
         </div>
        
      );
